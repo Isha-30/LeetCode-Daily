30fb9eacc84bffe8881f4bcef6c792dfec474b8f
Isha-30 <80475223+Isha-30@users.noreply.github.com>
2022-09-05 23:40:07 +0530

    Given an n-ary tree, return the level order traversal of its nodes' values.

---
+++ N-ary-Tree-Level-Order-Traversal.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new LinkedList<>();
+        
+        if (root == null) 
+            return res;
+        
+        Queue<Node> q = new LinkedList<>();
+        
+        q.offer(root);
+        
+        while (!q.isEmpty()) {
+            List<Integer> curLevel = new LinkedList<>();
+            
+            int len = q.size();
+            
+            for (int i = 0; i < len; i++) {
+                Node curr = q.poll();
+                curLevel.add(curr.val);
+                for (Node c : curr.children)
+                    q.add(c);
+            }
+            
+            res.add(curLevel);
+        }
+        
+        return res;
+    }
+}
